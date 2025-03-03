72f21285555399f31cb8cf3f6821fd41ab1598cd
frenki1004 <ffrenki94@gmail.com>
2025-03-03 01:32:45 +0100

    add everything

--- test.sh
+++ test.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
-userInput=$*
-echo "User input je $userInput"
+opcijice=$(ls ~/Music/hackspotify)
 
 
+while [ $# -gt 0 ]; do
+  arg=$1
+  case $arg in
+    option_1)
+     # do_option_1
+    ;;
+    option_2)
+     # do_option_2
+    ;;
+    shortlist)
+        for x in "${opcijice[@]}";do
+            echo $x
+        done
+    ;;
+    *)
+     echo Wrong option
+    ;;
+  esac
+  shift
+done
--- test2.sh
+++ test2.sh
@@ -1,6 +1,9 @@
 exists=true
 flag_u=false
-
-if [[ "$exists" = "false" || "$flag_u" = "true" ]];then
-    echo passed
-fi
+userInput="juice wrld 999 ep"
+    if [ -z ~/Music/hackspotify/"$userInput"/data.json ];then
+        echo "API key doesnt work or you didn't update, run all the same but with -u as in update"
+        exit 1
+    else
+        echo "postoji"
+    fi
